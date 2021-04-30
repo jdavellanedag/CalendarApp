9c093cb6a17f6e79565e040d4fb6ebbd73066808
jdavellanedag <juldave.gal@hotmail.com>
2021-04-29 22:28:17 -0500

    Added login and register connection to calendar-backend

--- src/types/types.js
+++ src/types/types.js
@@ -1,9 +1,18 @@
 export const types = {
   uiOpenModal: "[ui] Open modal",
   uiCloseModal: "[ui] Close modal",
+
   eventSetActive: "[event] Set active",
   eventAddNew: "[event] Add new",
   eventClearActiveEvent: "[event] Clrear active event",
   eventUpdated: "[event] Updated",
   eventDeleted: "[event] Deleted",
+
+  authChecking: "[auth] Checking login state",
+  authCheckingFinish: "[auth] Checking finished",
+  authStartLogin: "[auth] Start login",
+  authLogin: "[auth] Login",
+  authStartRegister: "[auth] Start register",
+  authStartTokenRenew: "[auth] Renew token",
+  authLogout: "[auth] Logout",
 };
